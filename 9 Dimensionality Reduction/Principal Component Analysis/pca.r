a46f303793629ccba492300049a6c41dbd3e9286
Minmay Upadhyay <uminmay@gmail.com>
2021-07-18 16:00:34 +0530

    added PCA in Dimensionality Reduction

---
+++ 9 Dimensionality Reduction/Principal Component Analysis/pca.r
@@ -0,0 +1,73 @@
+# Principal Component Analysis
+
+dataset = read.csv('Wine.csv')
+
+
+##TRAIN_TEST_SPLIT
+library(caTools)
+set.seed(121)
+split = sample.split(dataset$Customer_Segment, SplitRatio = 0.75)
+training_set = subset(dataset, split = TRUE)
+test_set = subset(dataset,split = FALSE)
+
+##FEATURE SCALING
+training_set[-14] = scale(training_set[-14])
+test_set[-14] = scale(test_set[-14])
+
+#Applying PCA
+library(caret)
+library(e1071)
+pca = preProcess(x = training_set[-14],
+                 method = "pca",
+                 pcaComp = 2)
+training_set = predict(pca, training_set)
+training_set = training_set[c(2:3,1)]
+test_set = predict(pca, test_set)
+test_set = test_set[c(2:3,1)]
+
+#apply svm
+classifier = svm(formula = Customer_Segment ~ .,
+                 data = training_set,
+                 type = 'C-classification',
+                 kernel = 'linear')
+
+# Predicting the Test set results
+y_pred = predict(classifier, newdata = test_set[-3])
+
+# Making the Confusion Matrix
+cm = table(test_set[, 3], y_pred)
+
+# Visualizing the Training set results
+library(ElemStatLearn)
+set = training_set
+X1 = seq(min(set[, 1]) - 1, max(set[, 1]) + 1, by = 0.01)
+X2 = seq(min(set[, 2]) - 1, max(set[, 2]) + 1, by = 0.01)
+grid_set = expand.grid(X1, X2)
+colnames(grid_set) = c('PC1', 'PC2')
+y_grid = predict(classifier, newdata = grid_set)
+plot(set[, -3],
+     main = 'SVM (Training set)',
+     xlab = 'PC1', ylab = 'PC2',
+     xlim = range(X1), ylim = range(X2))
+contour(X1, X2, matrix(as.numeric(y_grid), length(X1), length(X2)), add = TRUE)
+points(grid_set, pch = '.', col = ifelse(y_grid == 2, 'deepskyblue', 
+                                  ifelse(y_grid == 1, 'springgreen3', 'tomato')))
+points(set, pch = 21, bg = ifelse(set[, 3] == 2, 'blue3', 
+                                  ifelse(set[, 3] == 1, 'green4', 'red3')))
+
+# Visualising the Test set results
+library(ElemStatLearn)
+set = test_set
+X1 = seq(min(set[, 1]) - 1, max(set[, 1]) + 1, by = 0.01)
+X2 = seq(min(set[, 2]) - 1, max(set[, 2]) + 1, by = 0.01)
+grid_set = expand.grid(X1, X2)
+colnames(grid_set) = c('PC1', 'PC2')
+y_grid = predict(classifier, newdata = grid_set)
+plot(set[, -3], main = 'SVM (Test set)',
+     xlab = 'PC1', ylab = 'PC2',
+     xlim = range(X1), ylim = range(X2))
+contour(X1, X2, matrix(as.numeric(y_grid), length(X1), length(X2)), add = TRUE)
+points(grid_set, pch = '.', col = ifelse(y_grid == 2, 'deepskyblue', 
+                                  ifelse(y_grid == 1, 'springgreen3', 'tomato')))
+points(set, pch = 21, bg = ifelse(set[, 3] == 2, 'blue3', 
+                           ifelse(set[, 3] == 1, 'green4', 'red3')))
