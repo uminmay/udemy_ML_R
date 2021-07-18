2206a20edad0481bc334ca12157ca969ffa39bc8
Minmay Upadhyay <uminmay@gmail.com>
2021-07-18 20:00:41 +0530

    added KernelPCA with other corrections

--- 9 Dimensionality Reduction/Linear Discriminant Analysis/lda.r
+++ 9 Dimensionality Reduction/Linear Discriminant Analysis/lda.r
@@ -1,4 +1,4 @@
-# PCA
+# LDA
 
 # Importing the dataset
 dataset = read.csv('Wine.csv')
@@ -15,21 +15,18 @@ test_set = subset(dataset, split == FALSE)
 training_set[-14] = scale(training_set[-14])
 test_set[-14] = scale(test_set[-14])
 
-# Applying PCA
-# install.packages('caret')
-library(caret)
-# install.packages('e1071')
-library(e1071)
-pca = preProcess(x = training_set[-14], method = 'pca', pcaComp = 2)
-training_set = predict(pca, training_set)
-training_set = training_set[c(2, 3, 1)]
-test_set = predict(pca, test_set)
-test_set = test_set[c(2, 3, 1)]
+# Applying LDA
+library(MASS)
+lda = lda(formula = Customer_Segment ~ ., data = training_set)
+training_set = as.data.frame(predict(lda, training_set))
+training_set = training_set[c(5, 6, 1)]
+test_set = as.data.frame(predict(lda, test_set))
+test_set = test_set[c(5, 6, 1)]
 
 # Fitting SVM to the Training set
 # install.packages('e1071')
 library(e1071)
-classifier = svm(formula = Customer_Segment ~ .,
+classifier = svm(formula = class ~ .,
                  data = training_set,
                  type = 'C-classification',
                  kernel = 'linear')
@@ -46,11 +43,11 @@ set = training_set
 X1 = seq(min(set[, 1]) - 1, max(set[, 1]) + 1, by = 0.01)
 X2 = seq(min(set[, 2]) - 1, max(set[, 2]) + 1, by = 0.01)
 grid_set = expand.grid(X1, X2)
-colnames(grid_set) = c('PC1', 'PC2')
+colnames(grid_set) = c('x.LD1', 'x.LD2')
 y_grid = predict(classifier, newdata = grid_set)
 plot(set[, -3],
      main = 'SVM (Training set)',
-     xlab = 'PC1', ylab = 'PC2',
+     xlab = 'LD1', ylab = 'LD2',
      xlim = range(X1), ylim = range(X2))
 contour(X1, X2, matrix(as.numeric(y_grid), length(X1), length(X2)), add = TRUE)
 points(grid_set, pch = '.', col = ifelse(y_grid == 2, 'deepskyblue', ifelse(y_grid == 1, 'springgreen3', 'tomato')))
@@ -62,10 +59,10 @@ set = test_set
 X1 = seq(min(set[, 1]) - 1, max(set[, 1]) + 1, by = 0.01)
 X2 = seq(min(set[, 2]) - 1, max(set[, 2]) + 1, by = 0.01)
 grid_set = expand.grid(X1, X2)
-colnames(grid_set) = c('PC1', 'PC2')
+colnames(grid_set) = c('x.LD1', 'x.LD2')
 y_grid = predict(classifier, newdata = grid_set)
 plot(set[, -3], main = 'SVM (Test set)',
-     xlab = 'PC1', ylab = 'PC2',
+     xlab = 'LD1', ylab = 'LD2',
      xlim = range(X1), ylim = range(X2))
 contour(X1, X2, matrix(as.numeric(y_grid), length(X1), length(X2)), add = TRUE)
 points(grid_set, pch = '.', col = ifelse(y_grid == 2, 'deepskyblue', ifelse(y_grid == 1, 'springgreen3', 'tomato')))
